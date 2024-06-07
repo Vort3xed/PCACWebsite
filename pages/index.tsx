ecc070cfedbcc81f40d48d6c3ac70e9271dde5d4
Agneya T <agneyat2@gmail.com>
2024-06-07 16:52:30 -0400

    remove unneeded imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Layout from '../layouts/Main';
 import PageIntro from '../components/page-intro';
-import ProductsFeatured from '../components/products-featured';
 import Footer from '../components/footer';
-import Subscribe from '../components/subscribe';
 
 const IndexPage = () => {
   return (
