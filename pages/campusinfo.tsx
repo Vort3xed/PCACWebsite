1610aab88382d81e64728912e70d5e0f0061ad02
Agneya T <agneyat2@gmail.com>
2024-06-23 22:11:18 -0400

    entire website is done

--- pages/campusinfo.tsx
+++ pages/campusinfo.tsx
@@ -4,49 +4,58 @@ import Footer from '../components/footer';
 // import Timeline from '../components/Timeline';
 
 
-const AboutPage = () => {
+const CampusInfo = () => {
     return (
         <Layout>
             <PageIntro pageIntroBackgroundImage="url('/images/campusinfobg.png')" titleText='Get Involved'
                 subtitleText='Discover meaningful ways to engage with the Purdue community and contribute to our sustainability initiatives.' />
 
-                <section>
-                    <div className='getinvolved-items'>
-                        <div className='getinvolved-item'> 
-                            <h4>Sign the petition</h4>
-                            <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
-                            
-                        </div>
-                        <div className='getinvolved-item'> 
-                            <h4>Sign the petition</h4>
-                            <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
-                            <button>
-                                
-                            </button>
-                        </div>
+            <section>
+                <h4 className='banner-h4'>Metrics</h4>
+                <div className='getinvolved-items'>
+                    <div className='getinvolved-item'>
+                        <h4>Q:</h4>
+                        <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
+                        <br />
+                        <h4>A:</h4>
+                        <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
                     </div>
-                </section>
-
-            {/* <section>
+                    <div className='getinvolved-item'>
+                        <h4>Q:</h4>
+                        <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
+                        <br />
+                        <h4>A:</h4>
+                        <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
+                    </div>
+                    <div className='getinvolved-item'>
+                        <h4>Q:</h4>
+                        <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
+                        <br />
+                        <h4>A:</h4>
+                        <p>aiosjfpoase fioasjefoi ajsoie fjpa</p>
+                    </div>
+                    
+                </div>
+            </section>
+            <section>
 
                 <div className="about-content">
-                    <h4>What is the PCAC?</h4>
+                    <h4>Campus Information</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <h3>Mission and Vision</h3>
+                    <h3>Institute for a Sustainable Future</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <h3>Indiana Climate Change Impact Assessment</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <h3>Institute</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    
                     <p></p>
                     <p></p>
                 </div>
-            </section> */}
-
-            {/* <div className="contact-head">
-                <h4>Timeline</h4>
-            </div> */}
-
+            </section>
             <Footer />
         </Layout>
     )
 }
 
-export default AboutPage
+export default CampusInfo
