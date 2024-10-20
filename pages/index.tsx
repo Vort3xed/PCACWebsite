f465ec2533c16d92a537107457f1600be10ac30e
eileen <kohe@purdue.edu>
2024-10-20 00:50:56 -0400

    Add petition to main page (13)

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const IndexPage = () => {
         <section>
             <div className="about-content">
                 <h4>Sign Our 2024 Petition!</h4>
-                <h3><a style="text-align: center;"
+                <h3><a style="text-align: center;">
                     href="https://docs.google.com/forms/d/e/1FAIpQLSemvEx6L93lPnFK2cIua2uiex6o69vBMKM14UrPnNYTGAKKFQ/viewform"
                     target="_blank"
                     rel="noreferrer nofollow"><u>Click Here</u></a> </h3>
