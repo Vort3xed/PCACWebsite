4f55ad4893d0e9484745a76918ab2f48afe5d07d
eileen <kohe@purdue.edu>
2024-10-20 00:35:22 -0400

    Add petition to main page (8)

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ const IndexPage = () => {
         <section>
             <div className="about-content">
                 <h4>Sign our petition!</h4>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSemvEx6L93lPnFK2cIua2uiex6o69vBMKM14UrPnNYTGAKKFQ/viewform" target="_blank"
-                   rel="noreferrer nofollow">   Petition</a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSemvEx6L93lPnFK2cIua2uiex6o69vBMKM14UrPnNYTGAKKFQ/viewform" style="text-align:center" target="_blank"
+                   rel="noreferrer nofollow">Petition</a>
             </div>
         </section>
         {/* <section className="featured">
